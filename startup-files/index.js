750112ea7cb46190cee100e56982936512396228
Martin Faure <127661359+martinfaure@users.noreply.github.com>
2025-01-28 17:15:04 +0100

    feat(add):first-commit

--- startup-files/index.js
+++ startup-files/index.js
@@ -58,6 +58,15 @@ invent.appendChild(btn);
 
 const btn1 = document.createElement("button");
 const imgAdd2 = document.createElement("img");
+const case0 = document.getElementById("case0");
+const case1 = document.getElementById("case1");
+const case2 = document.getElementById("case2");
+const case3 = document.getElementById("case3");
+const case4 = document.getElementById("case4");
+const case5 = document.getElementById("case5");
+const case6 = document.getElementById("case6");
+const case7 = document.getElementById("case7");
+const case8 = document.getElementById("case8");
 
 imgAdd2.src = "./assets/iron-ingot.webp";
 imgAdd2.alt = "Iron-lingot";
@@ -65,3 +74,66 @@ imgAdd2.alt = "Iron-lingot";
 btn1.appendChild(imgAdd2);
 
 invent.appendChild(btn1);
+
+const imgAddCase0Iron = document.createElement("img");
+imgAddCase0Iron.src = "./assets/iron-ingot.webp";
+const imgAddCase0Stick = document.createElement("img");
+imgAddCase0Stick.src = "./assets/stick.webp";
+
+btn1.addEventListener("click", () => {
+	btn1.style.border = "3px, solid, red";
+
+	if (
+		case0.addEventListener("click", () => {
+			case0.appendChild(imgAddCase0Iron);
+		})
+	) {
+	}
+	if (
+		case1.addEventListener("click", () => {
+			case1.appendChild(imgAddCase0Iron);
+		})
+	) {
+	}
+});
+
+btn.addEventListener("click", () => {
+	btn.style.border = "3px, solid, red";
+
+	if (
+		case0.addEventListener("click", () => {
+			case0.appendChild(imgAddCase0Stick);
+		})
+	) {
+	}
+});
+
+let button = "";
+let image = "";
+let save = "";
+
+btn1.addEventListener("click", () => {
+	btn1.classList.add("active");
+});
+
+btn.addEventListener("click", () => {
+	btn.classList.add("active");
+});
+
+btn.addEventListener("click", () => {
+	if ((btn1.classList.contains = "active")) {
+		btn1.classList.remove = "active";
+		btn.classList.add = "active";
+		btn1.style.border = "none";
+		btn.style.border = "active";
+	}
+});
+
+btn1.addEventListener("click", () => {
+	if ((btn.classList.contains = "active")) {
+		btn.classList.remove = "active";
+		btn1.classList.add = "active";
+		btn.style.border = "none";
+		btn1.style.border = "active";
+	}
+});
